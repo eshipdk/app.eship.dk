fd25b8245b4e3f927754737268aaa5b4b9df525a
Rune Kok Nielsen <rune@runekoknielsen.dk>
2017-02-19 11:01:29 +0100

    Dynamic report granularity

--- app/util/reporting.rb
+++ app/util/reporting.rb
@@ -1,18 +1,41 @@
 
 module Reporting
   
+  def days_since_epoch ts
+    (ts.to_i / 86400).floor
+  end
+  
+  def wday ts
+    ts.wday == 0 ? 6 : ts.wday - 1
+  end
+  
+  
   def generate_booking_report from, to
     
-  
-    data = Shipment.group(['DATE_FORMAT(DATE(created_at), "%Y-%m-%d")', 'shipping_state > 1'])
+    n_days = days_since_epoch(to) - days_since_epoch(from)
+    
+    if n_days < 30
+      from = from
+      granularity = :day
+      group_by = 'DATE_FORMAT(DATE(created_at), "%Y-%m-%d")'      
+    elsif n_days < 200
+      from = from - (wday from) * 86400
+      granularity = :week
+      group_by = 'DATE_FORMAT(DATE(created_at) - WEEKDAY(created_at), "%Y-%m-%d")'
+    else
+      from = from  - (from.mday - 1) * 86400
+      granularity = :month
+      group_by = 'DATE_FORMAT(DATE(created_at) - DAYOFMONTH(created_at) + 1, "%Y-%m-%d")'
+    end
+    
+    data = Shipment.group([group_by, 'shipping_state > 1'])
             .where('created_at >= DATE(?)', from)
             .where('created_at <= DATE(?)', to)
             .count
-            
     res = {}
     date = from
     while date <= to
-      date = date + 1.days
+      
       date_s = date.strftime('%Y-%m-%d')
       
       initiated = data[[date_s, 0]].to_i
@@ -23,8 +46,16 @@ module Reporting
         'shipped' => shipped,
         'idle' => initiated
       }
+      
+      if granularity == :day
+        date = date + 1.days
+      elsif granularity == :week
+        date = date + 7.days
+      else
+        next_month = (date + 35.days)
+        date = next_month - (next_month.mday - 1) * 86400
+      end
     end
-
     return res
   end
   
